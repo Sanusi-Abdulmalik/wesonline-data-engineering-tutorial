dbbb2243fb4cbc7199810e58ce0410516d418abb
Osarenren bright omobude <dbrightosarenren@gmail.com>
2026-06-25 21:12:11 +0100

    Add files via upload

--- airflow/dags/cart_dag.py
+++ airflow/dags/cart_dag.py
@@ -1,1 +1,43 @@
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
 
+
+default_args = {
+    "owner": "bright",
+    "start_date": datetime(2025, 7, 2),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+with DAG(
+    dag_id="customer_carts",
+    description="fecthing customers cart records",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["customers", "carts", "airflowdags"],
+) as testdag:
+
+    start = EmptyOperator(
+        task_id='start')
+    
+    end = EmptyOperator(
+        task_id='end')
+
+
+    cart = BashOperator(
+        task_id="Cart_data",
+        bash_command="python /opt/airflow/scripts/ecom_sales_data.py cart"
+    )
+    cust = BashOperator(
+        task_id="Customer_data",
+        bash_command="python /opt/airflow/scripts/ecom_sales_data.py cust"
+    )
+    enrich = BashOperator(
+        task_id="Enrinched_data",
+        bash_command="python /opt/airflow/scripts/ecom_sales_data.py enrich"
+    )
+    start >> [cart, cust] >> enrich >> end
---
+++ airflow/dags/survery_data_dag.py
@@ -0,0 +1,39 @@
+# dags/bash_extract_dag.py
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.operators.empty import EmptyOperator
+
+default_args = {
+    'owner': 'wesonline',
+    'depends_on_past': False,
+    'retries': 1,
+}
+
+with DAG(
+    'survey_data',
+    default_args=default_args,
+    description='Run extract.py using BashOperator',
+    start_date=datetime(2026, 3, 1),
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+    
+    start = EmptyOperator(
+        task_id='start')
+
+    end = EmptyOperator(
+        task_id='end'
+    )
+
+
+    bronze_job = BashOperator(
+        task_id='bronze_data',
+        bash_command='python /opt/airflow/scripts/survey_data_pipeline.py 1'
+    )
+    silver_job = BashOperator(
+        task_id='silver_data',
+        bash_command='python /opt/airflow/scripts/survey_data_pipeline.py 2'
+    )
+
+    start >> bronze_job >> silver_job >> end
